d75427fba3b497c3538f932477cbfce87737d60c
jhalak <jhalak92upadhyay@gmail.com>
2022-12-26 15:32:52 +0530

    Added Firebase

--- lib/LOGIN.dart
+++ lib/LOGIN.dart
@@ -17,73 +17,75 @@ class _LoginState extends State<Login> {
         titleTextStyle: TextStyle(color: Colors.black,fontSize: 20.0,fontWeight: FontWeight.bold),
         centerTitle: true,
       ),
-      body: Column(
-        children:  [
-          const SizedBox(
-            height: 35.0,
-          ),
-          Hero(child: Image.asset('Images/logo.png'),tag: 'ani',),
-          const SizedBox(
-            height: 45.0,
-          ),
-          const TextField(
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                color: Colors.black
+      body: SingleChildScrollView(
+        child: Column(
+          children:  [
+            const SizedBox(
+              height: 35.0,
             ),
-            cursorColor: Colors.black,
-            decoration: InputDecoration(
-              hintText: 'Enter Mail Id',
-              hintStyle: TextStyle(color: Colors.black54),
-              filled: true,
-              fillColor: Colors.white,
-              icon: Icon(Icons.person),
-              iconColor: Colors.white,
-              border: OutlineInputBorder(
-                borderRadius:BorderRadius.all(Radius.circular(20),),
+            Hero(child: Image.asset('Images/logo.png'),tag: 'ani',),
+            const SizedBox(
+              height: 45.0,
+            ),
+            const TextField(
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.black
+              ),
+              cursorColor: Colors.black,
+              decoration: InputDecoration(
+                hintText: 'Enter Mail Id',
+                hintStyle: TextStyle(color: Colors.black54),
+                filled: true,
+                fillColor: Colors.white,
+                icon: Icon(Icons.person),
+                iconColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius:BorderRadius.all(Radius.circular(20),),
+                ),
               ),
             ),
-          ),
-          const SizedBox(
-            height: 35.0,
-          ),
-          const TextField(
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.black,
+            const SizedBox(
+              height: 35.0,
             ),
-            obscureText: true,
-            cursorColor: Colors.black,
-            decoration: InputDecoration(
-              hintText: 'Enter Password',
-              hintStyle: TextStyle(color: Colors.black54),
-              filled: true,
-              fillColor: Colors.white,
-              icon: Icon(Icons.lock),
-              iconColor: Colors.white,
-              border: OutlineInputBorder(
-                borderRadius:BorderRadius.all(Radius.circular(20),),
+            const TextField(
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black,
+              ),
+              obscureText: true,
+              cursorColor: Colors.black,
+              decoration: InputDecoration(
+                hintText: 'Enter Password',
+                hintStyle: TextStyle(color: Colors.black54),
+                filled: true,
+                fillColor: Colors.white,
+                icon: Icon(Icons.lock),
+                iconColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius:BorderRadius.all(Radius.circular(20),),
+                ),
               ),
             ),
-          ),
-          const SizedBox(
-            height: 35.0,
-          ),
-          Container(
-            height: 40.0,
-            width: 180.0,
-            margin: EdgeInsets.only(left: 40.0),
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(20)),
-              color: Colors.green,
+            const SizedBox(
+              height: 35.0,
             ),
-            child: TextButton(
-              onPressed: (){
-              },
-              child: Text('LOGIN',style: TextStyle(color: Colors.black),),
+            Container(
+              height: 40.0,
+              width: 180.0,
+              margin: EdgeInsets.only(left: 40.0),
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Colors.green,
+              ),
+              child: TextButton(
+                onPressed: (){
+                },
+                child: Text('LOGIN',style: TextStyle(color: Colors.black),),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/Signup.dart
+++ lib/Signup.dart
@@ -18,74 +18,76 @@ class _SignupState extends State<Signup> {
         titleTextStyle: TextStyle(color: Colors.black,fontSize: 20.0,fontWeight: FontWeight.bold),
         centerTitle: true,
       ),
-      body: Column(
-        children:  [
-          const SizedBox(
-            height: 35.0,
-          ),
-          Hero(child: Image.asset('Images/logo.png'),tag: 'ani',),
-             const SizedBox(
-               height: 45.0,
-             ),
-             const TextField(
-               textAlign: TextAlign.center,
-               style: TextStyle(
-                 color: Colors.black
+      body: SingleChildScrollView(
+        child: Column(
+          children:  [
+            const SizedBox(
+              height: 35.0,
+            ),
+            Hero(child: Image.asset('Images/logo.png'),tag: 'ani',),
+               const SizedBox(
+                 height: 45.0,
                ),
-               cursorColor: Colors.black,
+               const TextField(
+                 textAlign: TextAlign.center,
+                 style: TextStyle(
+                   color: Colors.black
+                 ),
+                 cursorColor: Colors.black,
+                decoration: InputDecoration(
+                  hintText: 'Enter Mail Id',
+                  hintStyle: TextStyle(color: Colors.black54),
+                  filled: true,
+                  fillColor: Colors.white,
+                  icon: Icon(Icons.person),
+                  iconColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius:BorderRadius.all(Radius.circular(20),),
+                  ),
+                ),
+              ),
+            const SizedBox(
+              height: 35.0,
+            ),
+            const TextField(
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black,
+              ),
+              obscureText: true,
+              cursorColor: Colors.black,
               decoration: InputDecoration(
-                hintText: 'Enter Mail Id',
+                hintText: 'Enter Password',
                 hintStyle: TextStyle(color: Colors.black54),
                 filled: true,
                 fillColor: Colors.white,
-                icon: Icon(Icons.person),
+                icon: Icon(Icons.lock),
                 iconColor: Colors.white,
                 border: OutlineInputBorder(
                   borderRadius:BorderRadius.all(Radius.circular(20),),
                 ),
               ),
             ),
-          const SizedBox(
-            height: 35.0,
-          ),
-          const TextField(
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.black,
+            const SizedBox(
+              height: 35.0,
             ),
-            obscureText: true,
-            cursorColor: Colors.black,
-            decoration: InputDecoration(
-              hintText: 'Enter Password',
-              hintStyle: TextStyle(color: Colors.black54),
-              filled: true,
-              fillColor: Colors.white,
-              icon: Icon(Icons.lock),
-              iconColor: Colors.white,
-              border: OutlineInputBorder(
-                borderRadius:BorderRadius.all(Radius.circular(20),),
+            Container(
+              height: 40.0,
+              width: 180.0,
+              margin: EdgeInsets.only(left: 40.0),
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Colors.green,
               ),
+              child: TextButton(
+                onPressed: (){
+                  Navigator.pushNamed(context, 'Login');
+                },
+                child: Text('SIGNUP',style: TextStyle(color: Colors.black),),
+                ),
             ),
-          ),
-          const SizedBox(
-            height: 35.0,
-          ),
-          Container(
-            height: 40.0,
-            width: 180.0,
-            margin: EdgeInsets.only(left: 40.0),
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(20)),
-              color: Colors.green,
-            ),
-            child: TextButton(
-              onPressed: (){
-                Navigator.pushNamed(context, 'Login');
-              },
-              child: Text('SIGNUP',style: TextStyle(color: Colors.black),),
-              ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
